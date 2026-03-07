0b89f8502bc7c8155c8fbb01ca0cfd7865a19be6
d3term1na <daniel.chanlokjing@gmail.com>
2026-03-07 21:55:38 +0800

    redemption data and home page UI

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"slices"
 	"strconv"
 	"time"
 )
 
 var tpl *template.Template
 var records [][]string
-var redemption_data []string
+var redemption_data [][]string
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	file, _ := os.Open("redeem_sample.csv")
 	defer file.Close()
@@ -26,8 +26,6 @@ func main() {
 
 	file, _ = os.Open("redemption_data.csv")
 	defer file.Close()
-	// redemption_data, _ = csv.NewReader(file).ReadAll()
-	// redemption_data = redemption_data[1:]
 	reader := csv.NewReader(file)
 	reader.Read()
 	for {
@@ -39,22 +37,37 @@ func main() {
 			log.Fatalf("Error while reading CSV file: %s", err)
 		}
 
-		// Process the data (e.g., print the fields)
-		redemption_data = append(redemption_data, row[0])
-		// Access individual fields by index
-		// fmt.Printf("Field 1: %s, Field 2: %s\n", record[0], record[1])
+		timestamp, err := strconv.ParseInt(row[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		t := time.UnixMilli(timestamp)
+		formatted := t.Local().Format("2006-01-02 15:04:05")
+		redemption_data = append(redemption_data, []string{row[0], formatted})
 	}
-
+	fmt.Println(redemption_data[0][0])
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/staff", staffHandler)
+	http.HandleFunc("/admin", adminHandler)
 	http.HandleFunc("/getId", getIdHandler)
+	http.HandleFunc("/getQR", getQRHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index.html", nil)
+	tpl.ExecuteTemplate(w, "index.html", redemption_data)
 }
+
+func staffHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "staff.html", nil)
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "admin.html", nil)
+}
+
 func getIdHandler(w http.ResponseWriter, r *http.Request) {
 	queryId := r.FormValue("id")
 	team_name := ""
@@ -64,22 +77,28 @@ func getIdHandler(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 	}
-	var result bool
-	if slices.Contains(redemption_data, team_name) {
-		result = false
-	} else {
-		redemption_data = append(redemption_data, team_name)
+	result := true
+	for _, row := range redemption_data {
+		if row[0] == team_name {
+			result = false
+		}
+	}
+	if result {
 		t := time.Now()
 		millis_val := t.UnixMilli()
 		millis_str := strconv.Itoa(int(millis_val))
+		redemption_data = append(redemption_data, []string{team_name, millis_str})
 		if err := addToCSV("redemption_data.csv", []string{team_name, millis_str}); err != nil {
 			log.Fatalf("Error appending to CSV: %v", err)
-			fmt.Println("hi")
 		}
-		result = true
 	}
 	tpl.ExecuteTemplate(w, "result.html", result)
 }
+
+// func getQRHandler(w http.ResponseWriter, r *http.Request) {
+// 	queryId := r.FormValue("id")
+
+// }
 
 func addToCSV(fileName string, record []string) error {
 	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
@@ -96,7 +115,6 @@ func addToCSV(fileName string, record []string) error {
 	}
 
 	writer.Flush()
-
 	if err = writer.Error(); err != nil {
 		return fmt.Errorf("Failed to flush data: %w", err)
 	}
