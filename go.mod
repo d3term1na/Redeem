987ccf3a6c8e872b544d28c1da75515a2140e8c8
d3term1na <daniel.chanlokjing@gmail.com>
2026-03-08 20:38:56 +0800

    implemented SQL database, brushed up UI

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26.1
 
 require (
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
 	github.com/maruel/rs v1.1.0 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/tuotoo/qrcode v0.0.0-20220425170535-52ccc2bebf5d // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/csv"
 	"fmt"
 	"html/template"
@@ -8,8 +9,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
+
+	_ "github.com/lib/pq"
 
 	qrcode "github.com/skip2/go-qrcode"
 	qrdecode "github.com/tuotoo/qrcode"
@@ -17,20 +19,27 @@ import (
 
 var tpl *template.Template
 var records [][]string
-var redemption_data [][]string
 var png []byte
 var err error
 var staffID string
+var DB *sql.DB
 
-func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+func initDB() {
+	connStr := "host=localhost user=postgres password=DanielChan12! dbname=redeemdb sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	DB = db
+
+	_, err4 := DB.Exec("TRUNCATE TABLE staff, teams CASCADE")
+	if err4 != nil {
+		panic(err4)
+	}
 
 	file, _ := os.Open("redeem_sample.csv")
-	defer file.Close()
-	records, _ = csv.NewReader(file).ReadAll()
-	records = records[1:]
-
-	file, _ = os.Open("redemption_data.csv")
 	defer file.Close()
 	reader := csv.NewReader(file)
 	reader.Read()
@@ -42,16 +51,69 @@ func main() {
 			}
 			log.Fatalf("Error while reading CSV file: %s", err)
 		}
-
-		timestamp, err := strconv.ParseInt(row[1], 10, 64)
+		var exists bool
+		team_name := row[1]
+		err = DB.QueryRow(
+			"SELECT EXISTS(SELECT 1 FROM teams WHERE team_name=$1)",
+			team_name,
+		).Scan(&exists)
 		if err != nil {
 			panic(err)
 		}
-		t := time.UnixMilli(timestamp)
-		formatted := t.Local().Format("2006-01-02 15:04:05")
-		redemption_data = append(redemption_data, []string{row[0], formatted})
+		if !exists {
+			_, err1 := DB.Exec(
+				"INSERT INTO teams (team_name, redeemed, redeemed_at) VALUES ($1,$2,$3)",
+				team_name,
+				false,
+				nil,
+			)
+			if err1 != nil {
+				panic(err1)
+			}
+		}
+		_, err2 := DB.Exec(
+			"INSERT INTO staff (staff_id, team_name, created_at) VALUES ($1,$2,$3)",
+			row[0],
+			team_name,
+			row[2],
+		)
+		if err2 != nil {
+			panic(err2)
+		}
 	}
-	fmt.Println(redemption_data[0][0])
+}
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	initDB()
+
+	// file, _ := os.Open("redeem_sample.csv")
+	// defer file.Close()
+	// records, _ = csv.NewReader(file).ReadAll()
+	// records = records[1:]
+
+	// file, _ = os.Open("redemption_data.csv")
+	// defer file.Close()
+	// reader := csv.NewReader(file)
+	// reader.Read()
+	// for {
+	// 	row, err := reader.Read()
+	// 	if err != nil {
+	// 		if err == io.EOF {
+	// 			break
+	// 		}
+	// 		log.Fatalf("Error while reading CSV file: %s", err)
+	// 	}
+
+	// 	timestamp, err := strconv.ParseInt(row[1], 10, 64)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	t := time.UnixMilli(timestamp)
+	// 	formatted := t.Local().Format("2006-01-02 15:04:05")
+	// 	redemption_data = append(redemption_data, []string{row[0], formatted})
+	// }
+	// fmt.Println(redemption_data[0][0])
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
 
 	http.HandleFunc("/", indexHandler)
@@ -74,6 +136,36 @@ func staffHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func adminHandler(w http.ResponseWriter, r *http.Request) {
+	type Team struct {
+		Name       string
+		Redeemed   bool
+		RedeemedAt *time.Time
+	}
+
+	rows, err := DB.Query("SELECT team_name, redeemed, redeemed_at FROM teams")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var redemption_data []Team
+
+	for rows.Next() {
+		var t Team
+		var redeemed_at_millis *int64
+		err := rows.Scan(&t.Name, &t.Redeemed, &redeemed_at_millis)
+		if err != nil {
+			panic(err)
+		}
+		if redeemed_at_millis != nil {
+			ts := time.UnixMilli(*redeemed_at_millis)
+			t.RedeemedAt = &ts
+		} else {
+			t.RedeemedAt = nil
+		}
+		redemption_data = append(redemption_data, t)
+	}
+
 	tpl.ExecuteTemplate(w, "admin.html", redemption_data)
 }
 
@@ -137,30 +229,48 @@ func decodeQRHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func resultHandler(w http.ResponseWriter, r *http.Request) {
-	team_name := ""
-	for _, record := range records {
-		if staffID == record[0] {
-			team_name = record[1]
-			break
+
+	type Result struct {
+		Valid    bool
+		Redeemed bool
+		Team     string
+	}
+
+	var team_name string
+	var valid bool
+	var redeemed bool
+	var res Result
+
+	err = DB.QueryRow(
+		"SELECT team_name FROM staff WHERE staff_id = $1",
+		staffID,
+	).Scan(&team_name)
+
+	if team_name == "" {
+		valid = false
+		redeemed = true
+	} else {
+		valid = true
+		err = DB.QueryRow(
+			"SELECT redeemed FROM teams WHERE team_name = $1",
+			team_name,
+		).Scan(&redeemed)
+		if !redeemed {
+			_, err1 := DB.Exec(
+				"UPDATE teams SET redeemed = $1, redeemed_at = $2 WHERE team_name = $3",
+				true,
+				time.Now().UnixMilli(), // BIGINT milliseconds
+				team_name,
+			)
+			if err1 != nil {
+				panic(err1)
+			}
 		}
 	}
-	result := true
-	for _, row := range redemption_data {
-		if row[0] == team_name {
-			result = false
-		}
-	}
-	if result {
-		t := time.Now()
-		formatted := t.Local().Format("2006-01-02 15:04:05")
-		millis_val := t.UnixMilli()
-		millis_str := strconv.Itoa(int(millis_val))
-		redemption_data = append(redemption_data, []string{team_name, formatted})
-		if err := addToCSV("redemption_data.csv", []string{team_name, millis_str}); err != nil {
-			log.Fatalf("Error appending to CSV: %v", err)
-		}
-	}
-	tpl.ExecuteTemplate(w, "result.html", result)
+	res.Valid = valid
+	res.Redeemed = redeemed
+	res.Team = team_name
+	tpl.ExecuteTemplate(w, "result.html", res)
 }
 
 func addToCSV(fileName string, record []string) error {
